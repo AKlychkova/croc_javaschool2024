1dacbe263342408808246de47fd0e78ce6fc9fe5
AKlychkova <adklychkova@edu.hse.ru>
2024-03-26 22:59:18 +0300

    Fix #3

--- csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task6/IllegalMoveException.java
+++ csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task6/IllegalMoveException.java
@@ -9,14 +9,24 @@ package ru.croc.javaschool2024.Klychkova.task6;
 public class IllegalMoveException extends Exception {
     private final ChessPosition from;
     private final ChessPosition to;
+    private final String message;
 
     public IllegalMoveException(ChessPosition from, ChessPosition to) {
         this.from = from;
         this.to = to;
+        message = String.format("%s -> %s", from.toString(),to.toString());
+    }
+
+    public ChessPosition getFrom() {
+        return from;
+    }
+
+    public ChessPosition getTo() {
+        return to;
     }
 
     @Override
     public String getMessage() {
-        return String.format("%s -> %s", from.toString(),to.toString());
+        return message;
     }
 }
