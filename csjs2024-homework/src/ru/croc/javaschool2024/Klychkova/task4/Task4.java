e47cef6c774e4711b66036d505407fcd0f3d88a9
AKlychkova <adklychkova@edu.hse.ru>
2024-03-26 14:24:44 +0300

    Fix task 4

--- csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task4/Task4.java
+++ csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task4/Task4.java
@@ -1,55 +1,70 @@
 package ru.croc.javaschool2024.Klychkova.task4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Math.min;
+
 public class Task4 {
     public static void main(String[] args) {
-        String source = "..."; // test data
+        String source = """
+                /*
+                 * My first ever program in Java!
+                 */
+                class Hello { // class body starts here
+                 
+                  /* main method */
+                  public static void main(String[] args/* we put command line arguments here*/) {
+                    // this line prints my first greeting to the screen
+                    System.out.println("Hi!"); // :)
+                  }
+                } // the end
+                // to be continued...
+                """; // test data
         String noComments = removeJavaComments(source);
         System.out.println(noComments);
     }
 
+    private static int minPositive(int... values) {
+        int res = Integer.MAX_VALUE;
+        for (int value : values) {
+            if (value >= 0) {
+                res = min(res, value);
+            }
+        }
+        if (res == Integer.MAX_VALUE) {
+            res = -1;
+        }
+        return res;
+    }
+
 
     public static String removeJavaComments(String input) {
-        //  Solution with regular expressions (works only on test 1):
-        //  Pattern pattern = Pattern.compile("(/\\*[\\s\\S]*?\\*/)|(//.*)");
-        //  Matcher matcher = pattern.matcher(input);
-        //  return matcher.replaceAll("");
-
-        // Flag to indicate that string have started or not.
-        int quotes = 0;
-        // Flags to indicate that single line and multiple line comments have started or not.
-        boolean s_cmt = false;
-        boolean m_cmt = false;
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < input.length(); ++i) {
-            if (input.charAt(i) == '\"') {
-                quotes = (quotes + 1) % 2;
+
+        int current_position = 0;
+        int quotes = input.indexOf('\"', current_position);
+        int s_cmt = input.indexOf("//", current_position);
+        int m_cmt = input.indexOf("/*", current_position);
+
+        while (s_cmt > 0 || m_cmt > 0) {
+            int first = minPositive(quotes, s_cmt, m_cmt);
+            if (first == quotes) {
+                int close_quotes = input.indexOf('\"',quotes+1);
+                result.append(input, current_position, close_quotes+1);
+                current_position = close_quotes + 1;
+            } else if (first == s_cmt) {
+                result.append(input, current_position, s_cmt);
+                current_position = input.indexOf('\n',s_cmt);
+            } else {
+                result.append(input, current_position, m_cmt);
+                current_position = input.indexOf("*/",m_cmt) + 2;
             }
-            // If single line comment flag is on, then check for end of it.
-            if (s_cmt && input.charAt(i) == '\n') {
-                s_cmt = false;
-                result.append(input.charAt(i));
-            }
-            // If multiple line comment is on, then check for end of it.
-            else if (m_cmt && input.charAt(i) == '*' && input.charAt(i+1) == '/') {
-                m_cmt = false;
-                i++;
-            }
-            // If this character is in a comment, ignore it.
-            else if (s_cmt || m_cmt) {
-                continue;
-            }
-            // Check for beginning of comments and set the appropriate flags.
-            else if (input.charAt(i) == '/' && input.charAt(i+1) == '/' && quotes == 0) {
-                s_cmt = true;
-                i++;
-            }
-            else if (input.charAt(i) == '/' && input.charAt(i+1) == '*' && quotes == 0) {
-                m_cmt = true;
-                i++;
-            }
-            // If current character is a non-comment character, append it to res.
-            else result.append(input.charAt(i));
+            quotes = input.indexOf('\"', current_position);
+            s_cmt = input.indexOf("//", current_position);
+            m_cmt = input.indexOf("/*", current_position);
         }
+        result.append(input,current_position,input.length());
         return result.toString();
     }
 }
