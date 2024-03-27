be49102cb61b823709381835a5f36b551ebb0642
AKlychkova <adklychkova@edu.hse.ru>
2024-03-27 12:22:29 +0300

    Fix #5

--- csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task6/KnightsMoveCheckerFactory.java
+++ csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task6/KnightsMoveCheckerFactory.java
@@ -9,6 +9,7 @@ import static java.lang.Math.abs;
  * @author Dmitry Malenok
  */
 public final class KnightsMoveCheckerFactory {
+    private static KnightsMoveChecker checker = null;
 
     /**
      * Конструктор.
@@ -24,25 +25,29 @@ public final class KnightsMoveCheckerFactory {
      * @return обработчик, проверяющий, что последовательность клеток на шахматной доске может быть пройдена ходом коня
      */
     public static KnightsMoveChecker get() {
-        return new KnightsMoveChecker() {
-            private boolean checkMove(ChessPosition from, ChessPosition to) {
-                return (abs(to.x() - from.x()) == 2 && abs(to.y() - from.y()) == 1) ||
-                        (abs(to.x() - from.x()) == 1 && abs(to.y() - from.y()) == 2);
-            }
 
-            @Override
-            public void check(String[] positions) throws IllegalMoveException {
-                for (int i = 0; i < positions.length - 1; ++i) {
-                    ChessPosition from = ChessPositionParser.parse(positions[i]);
-                    ChessPosition to = ChessPositionParser.parse(positions[i + 1]);
-                    if (!checkMove(from, to)) {
-                        throw new IllegalMoveException(from,to);
+        if (checker == null) {
+            checker = new KnightsMoveChecker() {
+                private boolean checkMove(ChessPosition from, ChessPosition to) {
+                    return (abs(to.x() - from.x()) == 2 && abs(to.y() - from.y()) == 1) ||
+                            (abs(to.x() - from.x()) == 1 && abs(to.y() - from.y()) == 2);
+                }
+
+                @Override
+                public void check(String[] positions) throws IllegalMoveException {
+                    for (int i = 0; i < positions.length - 1; ++i) {
+                        ChessPosition from = ChessPositionParser.parse(positions[i]);
+                        ChessPosition to = ChessPositionParser.parse(positions[i + 1]);
+                        if (!checkMove(from, to)) {
+                            throw new IllegalMoveException(from, to);
+                        }
+                    }
+                    if (positions.length == 1) {
+                        ChessPositionParser.parse(positions[0]);
                     }
                 }
-                if (positions.length == 1) {
-                    ChessPositionParser.parse(positions[0]);
-                }
-            }
-        };
+            };
+        }
+        return checker;
     }
 }
