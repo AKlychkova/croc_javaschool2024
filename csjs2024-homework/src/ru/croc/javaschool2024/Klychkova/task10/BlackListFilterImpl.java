f8008c22145806f1e58ca9de71872f7e864e1bc4
AKlychkova <adklychkova@edu.hse.ru>
2024-04-06 14:56:29 +0300

    Fix task 10 solution

--- csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task10/BlackListFilterImpl.java
+++ csjs2024-homework/src/ru/croc/javaschool2024/Klychkova/task10/BlackListFilterImpl.java
@@ -3,12 +3,16 @@ package ru.croc.javaschool2024.Klychkova.task10;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BlackListFilterImpl implements BlackListFilter {
     private boolean checkComment(String comment, Collection<String> blackList) {
+        Set<String> blackSet = blackList.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
         var words = List.of(comment.toLowerCase().split("[\\s-.?!)(,:;]"));
-        for (String blackWord : blackList) {
-            if (words.contains(blackWord.toLowerCase())) {
+        for (String word : words) {
+            if (blackSet.contains(word)) {
                 return false;
             }
         }
